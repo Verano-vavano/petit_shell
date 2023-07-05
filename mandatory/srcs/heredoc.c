dd66e53bd72c528d520ac53444ae375abea47bf8
Hugo <hdupire@students.42nice.fr>
2023-07-05 10:32:02 +0200

    here_docs et norme. les here-docs fonctionnent bien, bonne nouvelle ! et tout est norme a part deux wrong scope comments (normal) et une ligne trop longue, mais en meme temps, voila quoi. bon, go commencer le parsing, grosse flemme. le pire ca va etre les expansions de la commande et je sais pas quoi et puis le patati et le patata, enfin bref go bosser. ce que je veux dire avec ce commit, c'est un sens un peu cache de la vie tel que la normination et la here-doc-isation peuvent seeulement representer quune petite partie de la journee. je me demande s'il y a une limite de taille aux messages de commit. bon au revoir

--- mandatory/srcs/heredoc.c
+++ mandatory/srcs/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: hdupire <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/08 14:31:39 by hdupire           #+#    #+#             */
-/*   Updated: 2023/07/04 17:44:11 by hdupire          ###   ########.fr       */
+/*   Updated: 2023/07/05 10:18:24 by hdupire          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,7 +54,27 @@ void	unlink_heredocs(t_command *cmd)
 	}
 }
 
-static int	create_heredoc(int index)
+static void	write_heredoc(int fd, char *eof)
+{
+	char	*line;
+
+	line = 0;
+	while (1)
+	{
+		line = get_next_line(STDIN_FILENO);
+		line[ft_strchr_int(line, '\n')] = 0;
+		if (!ft_strcmp(line, eof))
+		{
+			free(line);
+			break ;
+		}
+		line[ft_strchr_int(line, '\n')] = '\n';
+		write(fd, line, ft_strlen(line));
+		free(line);
+	}
+}
+
+static int	create_heredoc(int index, char *eof)
 {
 	int		fd_heredoc;
 	char	*itoaed;
@@ -62,13 +82,14 @@ static int	create_heredoc(int index)
 
 	itoaed = ft_itoa(index);
 	file_name = ft_strjoin(TEMP, itoaed);
-	if (access(file_name, F_OK))
+	if (!access(file_name, F_OK))
 		unlink(file_name);
 	fd_heredoc = open(file_name, O_CREAT | O_WRONLY | O_APPEND, 0666);
 	if (fd_heredoc <= 0)
 		return (1);
 	free(itoaed);
 	free(file_name);
+	write_heredoc(fd_heredoc, eof);
 	return (0);
 }
 
@@ -81,12 +102,12 @@ int	here_doc(t_command *cmd)
 	{
 		if (cmd->purpose == HERE_DOC_DELIM)
 		{
-			create_heredoc(i);
+			create_heredoc(i, cmd->content);
 			i++;
 		}
 		cmd = cmd->next;
 	}
 	if (cmd->purpose == HERE_DOC_DELIM)
-		create_heredoc(i);
+		create_heredoc(i, cmd->content);
 	return (0);
 }
---
+++ mandatory/srcs/metal_injection.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   metal-injection.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hdupire <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/05 10:26:10 by hdupire           #+#    #+#             */
+/*   Updated: 2023/07/05 10:26:11 by hdupire          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "shellpticflesh.h"
+
+static int	rand_num(void)
+{
+	int		tmp_file;
+	int		rand;
+	int		pid;
+	char	c;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		tmp_file = open("/tmp/s-hell", O_CREAT | O_WRONLY | O_TRUNC, 0777);
+		dup2(tmp_file, STDOUT_FILENO);
+		close(tmp_file);
+		execve("/usr/bin/jot", (char *[]){"jot", "-r", "1", "0", "9", 0}, 0);
+		return (0);
+	}
+	else
+	{
+		waitpid(pid, 0, 0);
+		tmp_file = open("/tmp/s-hell", O_RDONLY);
+		read(tmp_file, &c, sizeof(c));
+		rand = (c - '0') + 1;
+		close(tmp_file);
+		unlink("/tmp/s-hell");
+		return (rand);
+	}
+}
+
+void	metal_injection(void)
+{
+	int		group_num;
+	int		i;
+	int		fd;
+	char	*group;
+
+	i = 0;
+	group_num = rand_num();
+	fd = open("/Users/tcharanc/code/minishell/mandatory/shell-on-earth.txt", O_RDONLY);
+	while (i < group_num)
+	{
+		group = get_next_line(fd);
+		i++;
+	}
+	printf("%s", group);
+}
