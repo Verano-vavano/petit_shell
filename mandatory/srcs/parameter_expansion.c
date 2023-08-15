be5910376f60f727762e0a3cc7fa4296fff90d2a
thibault <code@nigh.one>
2023-08-15 13:09:37 +0200

    commit avant d'aller manger, debut de param extension qui semble etre en bonne voie, cd $HOME marche et ca semble cool mais j'ai encore un coredump a regler si jamais la variable n'existe pas de toute maniere tu peux lire les todos qui sont au dessus de la fonction expand_variable dans le fichier parameter_expansion.c

--- mandatory/srcs/parameter_expansion.c
+++ mandatory/srcs/parameter_expansion.c
@@ -6,14 +6,44 @@
 /*   By: hdupire <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/13 15:17:36 by hdupire           #+#    #+#             */
-/*   Updated: 2023/08/13 15:19:34 by hdupire          ###   ########.fr       */
+/*   Updated: 2023/08/15 13:07:17 by tcharanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include "shellpticflesh.h"
+
+// if not found expand to \0
+// TODO pour le moment l'expansion ne prend que la premiere valeur du char **
+// mais que se passe t'il si on cherche a expand $PATH
+// faire des strcat en boucle?
+// TODO fix coredump quand je fais cette commande
+// CD $DKASJDLKAJ
+// la variable n'existe pas et ca cause un probleme
+void	expand_variable(t_command *cmd, t_env *env)
+{
+	char *variable;
+	char **new_val;
+
+	variable = cmd->content + 1;
+	if (env_contain(variable, env))
+		new_val = env_getval(variable, env);
+	else
+		new_val = NULL;
+	free(cmd->content);
+	cmd->content = NULL;
+	if (new_val)
+		cmd->content = ft_strdup(new_val[0]);
+}
 
 void	parameter_expansion(t_command *cmd, t_env *env)
 {
-	(void) cmd;
-	(void) env;
+
+	printf("param expansion, cmd = %s\n",cmd->content);
+	while(cmd)
+	{
+		if (*cmd->content == '$')
+			expand_variable(cmd, env);
+		cmd = cmd->next;
+	}
 }
