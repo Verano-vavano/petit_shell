d9065222c1d14796d9c8ab735bb25f5d86c545a2
Hugo Dupire <hugod1383@gmail.com>
2023-08-30 16:49:35 +0200

    Je me déclare génie. Signaux chiants bien foutus : SIGINT dans le heredoc marche et SIGQUIT dans le main n'apparaît pas

--- mandatory/srcs/heredoc.c
+++ mandatory/srcs/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: hdupire <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/08 14:31:39 by hdupire           #+#    #+#             */
-/*   Updated: 2023/08/29 19:22:36 by hdupire          ###   ########.fr       */
+/*   Updated: 2023/08/30 15:51:38 by hdupire          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int	get_heredoc_file(int hd, int mode)
 	if (mode == READ)
 		fd = open(filename, O_RDONLY);
 	else if (mode == WRITE)
-		fd = open(filename, O_CREAT | O_WRONLY | O_APPEND, 0666);
+		fd = open(filename, O_CREAT | O_WRONLY, 0666);
 	free(filename);
 	return (fd);
 }
@@ -66,7 +66,7 @@ static void	write_heredoc(int fd, char *eof, t_env *env)
 
 	eof = quote_removal(ft_strdup(eof));
 	lines = 1;
-	while (g_sig_rec != SIGINT)
+	while (true)
 	{
 		line = new_prompt(2, env);
 		if (!line)
@@ -94,10 +94,10 @@ static int	heredoc_child(int fd, char *eof, t_env *env)
 
 	pid = fork();
 	if (pid == -1)
-		return (0);
+		return (1);
 	else if (pid == 0)
 	{
-		signal(SIGINT, SIG_DFL);
+		signal(SIGINT, heredoc_handle);
 		write_heredoc(fd, eof, env);
 		exit(0);
 	}
@@ -106,26 +106,28 @@ static int	heredoc_child(int fd, char *eof, t_env *env)
 		if (g_sig_rec == SIGINT)
 		{
 			kill(pid, SIGINT);
-			printf("\n");
+			waitpid(pid, 0, 0);
 			g_sig_rec = 0;
 			return (130);
 		}
 		continue ;
 	}
-	return (1);
+	return (0);
 }
 
 static int	create_heredoc(int index, t_command *cmd, t_env *env)
 {
-	int		fd_heredoc;
+	int	fd_heredoc;
+	int	ret;
 
 	fd_heredoc = get_heredoc_file(index, WRITE);
 	if (fd_heredoc <= 0)
 		return (1);
 	if (cmd->purpose == HERE_DOC_DELIM)
 	{
-		if (!heredoc_child(fd_heredoc, cmd->content, env))
-			return (1);
+		ret = heredoc_child(fd_heredoc, cmd->content, env);
+		if (ret)
+			return (ret);
 	}
 	else
 	{
@@ -140,18 +142,22 @@ static int	create_heredoc(int index, t_command *cmd, t_env *env)
 int	here_doc(t_command *cmd, t_env *env)
 {
 	int	i;
+	int	ret;
 
 	i = 0;
+	ret = 0;
 	while (cmd->next)
 	{
 		if (cmd->purpose == HERE_DOC_DELIM || cmd->purpose == HERE_STRING)
 		{
-			create_heredoc(i, cmd, env);
+			ret = create_heredoc(i, cmd, env);
+			if (ret)
+				return (ret);
 			i++;
 		}
 		cmd = cmd->next;
 	}
 	if (cmd->purpose == HERE_DOC_DELIM || cmd->purpose == HERE_STRING)
-		create_heredoc(i, cmd, env);
-	return (0);
+		ret = create_heredoc(i, cmd, env);
+	return (ret);
 }
