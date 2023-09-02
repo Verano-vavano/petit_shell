658c8aca013ed56ba1bcdc189aa73d3f734d2d74
Hugo Dupire <hugod1383@gmail.com>
2023-09-02 23:58:13 +0200

    petite normination. Bientôt fini hihi

---
+++ mandatory/srcs/check_path.c
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_path.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hdupire <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/02 23:46:50 by hdupire           #+#    #+#             */
+/*   Updated: 2023/09/02 23:49:59 by hdupire          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "shellpticflesh.h"
+
+static int	check_pathing(char *cmd)
+{
+	int		last_s;
+	int		next_s;
+	char	replaced_char;
+
+	last_s = (cmd[0] == '/');
+	next_s = ft_strchr_int(cmd + last_s, '/');
+	while (cmd[last_s + next_s])
+	{
+		replaced_char = cmd[last_s + next_s];
+		cmd[last_s + next_s] = 0;
+		if (access(cmd, X_OK) != 0)
+		{
+			cmd[last_s + next_s] = replaced_char;
+			return (1);
+		}
+		cmd[last_s + next_s] = replaced_char;
+		last_s += next_s + 1;
+		next_s = ft_strchr_int(cmd + last_s, '/');
+	}
+	return (0);
+}
+
+char	*add_start(char *cmd)
+{
+	char	*n_cmd;
+
+	n_cmd = ft_strjoin("./", cmd);
+	if (!n_cmd)
+		return (cmd);
+	free(cmd);
+	return (n_cmd);
+}
+
+int	check_org_path(char *cmd)
+{
+	int	pathing;
+
+	if (cmd[ft_strchr_int(cmd, '/')] != '/')
+		return (0);
+	pathing = check_pathing(cmd);
+	if (access(cmd, F_OK) == 0)
+	{
+		if (access(cmd, X_OK) == 0 && !pathing)
+			return (-1);
+		else if (access(cmd, X_OK) != 0 || pathing)
+			return (126);
+		return (127);
+	}
+	return (0);
+}
+
+char	*check_path(char **paths, char *path_cmd)
+{
+	char	*full_path;
+
+	full_path = ft_strjoin(*paths, path_cmd);
+	if (full_path == 0)
+		return (0);
+	if (access(full_path, X_OK) == 0)
+	{
+		free(path_cmd);
+		return (full_path);
+	}
+	free(full_path);
+	return (0);
+}
+
+bool	check_builtin(char *cmd, bool is_rel)
+{
+	if (is_rel)
+		return (false);
+	else if (!(ft_strcmp("hell", cmd)
+			&& ft_strcmp("exit", cmd)
+			&& ft_strcmp("echo", cmd)
+			&& ft_strcmp("env", cmd)
+			&& ft_strcmp("export", cmd)
+			&& ft_strcmp("unset", cmd)
+			&& ft_strcmp("cd", cmd)
+			&& ft_strcmp("pwd", cmd)
+			&& ft_strcmp("tetris", cmd)))
+		return (true);
+	return (false);
+}
--- mandatory/srcs/get_path.c
+++ mandatory/srcs/get_path.c
@@ -6,108 +6,43 @@
 /*   By: hdupire <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/15 19:08:42 by hdupire           #+#    #+#             */
-/*   Updated: 2023/08/10 14:32:40 by hdupire          ###   ########.fr       */
+/*   Updated: 2023/09/02 23:48:40 by hdupire          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shellpticflesh.h"
 
-static int	check_pathing(char *cmd)
+static int	rel_search(t_process_cmd *cmd, char **path)
 {
-	int		last_s;
-	int		next_s;
-	char	replaced_char;
-
-	last_s = (cmd[0] == '/');
-	next_s = ft_strchr_int(cmd + last_s, '/');
-	while (cmd[last_s + next_s])
-	{
-		replaced_char = cmd[last_s + next_s];
-		cmd[last_s + next_s] = 0;
-		if (access(cmd, X_OK) != 0)
-		{
-			cmd[last_s + next_s] = replaced_char;
-			return (1);
-		}
-		cmd[last_s + next_s] = replaced_char;
-		last_s += next_s + 1;
-		next_s = ft_strchr_int(cmd + last_s, '/');
-	}
-	return (0);
-}
-
-static char	*add_start(char *cmd)
-{
-	char	*n_cmd;
-
-	n_cmd = ft_strjoin("./", cmd);
-	if (!n_cmd)
-		return (cmd);
-	free(cmd);
-	return (n_cmd);
-}
-
-static int	check_org_path(char *cmd)
-{
-	int	pathing;
-
-	if (cmd[ft_strchr_int(cmd, '/')] != '/')
-		return (0);
-	pathing = check_pathing(cmd);
-	if (access(cmd, F_OK) == 0)
-	{
-		if (access(cmd, X_OK) == 0 && !pathing)
-			return (-1);
-		else if (access(cmd, X_OK) != 0 || pathing)
-			return (126);
-		return (127);
-	}
-	return (0);
-}
-
-static char	*check_path(char **paths, char *path_cmd)
-{
+	char	*path_cmd;
 	char	*full_path;
 
-	full_path = ft_strjoin(*paths, path_cmd);
-	if (full_path == 0)
-		return (0);
-	if (access(full_path, X_OK) == 0)
+	path_cmd = ft_strjoin("/", cmd->cmd_name);
+	if (path_cmd == 0)
+		return (1);
+	while (*path)
 	{
-		free(path_cmd);
-		return (full_path);
+		full_path = check_path(path, path_cmd);
+		if (full_path)
+		{
+			cmd->free_name = true;
+			cmd->cmd_name = full_path;
+			return (0);
+		}
+		path++;
 	}
-	free(full_path);
-	return (0);
-}
-
-static bool	check_builtin(char *cmd, bool is_rel)
-{
-	if (is_rel)
-		return (false);
-	else if (!(ft_strcmp("hell", cmd)
-			&& ft_strcmp("exit", cmd)
-			&& ft_strcmp("echo", cmd)
-			&& ft_strcmp("env", cmd)
-			&& ft_strcmp("export", cmd)
-			&& ft_strcmp("unset", cmd)
-			&& ft_strcmp("cd", cmd)
-			&& ft_strcmp("pwd", cmd)
-			&& ft_strcmp("tetris", cmd)))
-		return (true);
-	return (false);
+	free(path_cmd);
+	return (-1);
 }
 
 // -1 = 0 = pas d'erreur
 // > 0 = erreur
-// .-1 = malloc
-// .-126 = perm denied
-// .-127 = not found
+// . 1 = malloc
+// . 126 = perm denied
+// . 127 = not found
 int	get_cmd_path(t_process_cmd *cmd, t_env *env)
 {
 	char	**path;
-	char	*path_cmd;
-	char	*full_path;
 	int		err_catcher;
 	bool	is_rel;
 
@@ -125,20 +60,8 @@ int	get_cmd_path(t_process_cmd *cmd, t_env *env)
 		return (command_error(cmd->cmd[0], err_catcher));
 	else if (!env_isdefined("PATH", env) || is_rel)
 		return (command_error(cmd->cmd[0], 127));
-	path_cmd = ft_strjoin("/", cmd->cmd_name);
-	if (path_cmd == 0)
-		return (1);
-	while (*path)
-	{
-		full_path = check_path(path, path_cmd);
-		if (full_path)
-		{
-			cmd->free_name = true;
-			cmd->cmd_name = full_path;
-			return (0);
-		}
-		path++;
-	}
-	free(path_cmd);
+	err_catcher = rel_search(cmd, path);
+	if (err_catcher != -1)
+		return (err_catcher);
 	return (command_error(cmd->cmd[0], 127));
 }
