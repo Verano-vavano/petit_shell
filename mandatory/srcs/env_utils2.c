caa3e60f78f35e5e7c8713547a235e5bdd464d64
Verano <hugod1383@gmail.com>
2023-09-10 13:49:14 +0200

    Résolution d'un bug qui faisait qu'on ne pouvait pas désassigner les variables exportées (chelou je sais). Et aussi résolution d'un autre bug qui faisait que le += n'ajoutait qu'à la première valeur

--- mandatory/srcs/env_utils2.c
+++ mandatory/srcs/env_utils2.c
@@ -6,7 +6,7 @@
 /*   By: tcharanc <code@nigh.one>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 16:05:16 by tcharanc          #+#    #+#             */
-/*   Updated: 2023/09/05 16:28:30 by hdupire          ###   ########.fr       */
+/*   Updated: 2023/09/10 13:44:15 by hdupire          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,17 +28,63 @@ t_env	*env_getptr(char *key, t_env *env)
 	return (ret);
 }
 
+static char	*re_char_etoilise(t_env *ptr)
+{
+	char	*ret;
+	size_t	total_len;
+	int		index;
+	int		i_ret;
+	int		i_val;
+
+	index = 0;
+	total_len = 0;
+	while (ptr->value[index])
+	{
+		total_len += ft_strlen(ptr->value[index]);
+		total_len++;
+		index++;
+	}
+	ret = ft_calloc(total_len, sizeof (char));
+	if (!ret)
+		return (0);
+	i_ret = 0;
+	index = 0;
+	while (ptr->value[index])
+	{
+		i_val = 0;
+		while (ptr->value[index][i_val])
+		{
+			ret[i_ret] = ptr->value[index][i_val];
+			i_val++;
+			i_ret++;
+		}
+		ret[i_ret] = ':';
+		i_ret++;
+		index++;
+	}
+	ret[i_ret - 1] = 0;
+	return (ret);
+}
+
 static void	env_change_val_rfunk(t_env *ptr, char *value, bool add)
 {
 	char	*temp;
+	char	*temp2;
 
 	if (add)
 	{
 		if (ptr->value && ptr->value[0])
 		{
-			temp = ft_strjoin(ptr->value[0], value);
-			free(ptr->value[0]);
-			ptr->value[0] = temp;
+			temp = re_char_etoilise(ptr);
+			if (!temp)
+				return ;
+			temp2 = ft_strjoin(temp, value);
+			free(temp);
+			if (!temp2)
+				return ;
+			free_char_etoile_etoile(ptr->value);
+			ptr->value = ft_split(temp2, ':');
+			free(temp2);
 		}
 		else
 			ptr->value[0] = ft_strdup(value);
@@ -64,6 +110,12 @@ void	env_change_val(char *key, char *value, t_env *env, bool is_exp)
 	ptr = env_getptr(key, env);
 	if (value)
 		env_change_val_rfunk(ptr, value, add);
+	else if (!add)
+	{
+		free_char_etoile_etoile(ptr->value);
+		ptr->value = ft_calloc(2, sizeof (char *));
+		ptr->value[0] = ft_calloc(1, sizeof (char));
+	}
 	if (is_exp)
 		ptr->is_exported = true;
 }
