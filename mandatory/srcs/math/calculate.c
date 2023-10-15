97ca5f3cf9517469207a5ab89f637c658969f9dd
thibault <code@nigh.one>
2023-10-15 09:22:49 +0200

    gestions de priorite des div et multiplications! Et maintenant n'importe quelle taille de calcul marche!

--- mandatory/srcs/math/calculate.c
+++ mandatory/srcs/math/calculate.c
@@ -6,30 +6,60 @@
 /*   By: tcharanc <code@nigh.one>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 16:13:15 by tcharanc          #+#    #+#             */
-/*   Updated: 2023/10/10 17:48:14 by tcharanc         ###   ########.fr       */
+/*   Updated: 2023/10/15 09:19:09 by tcharanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "math.h"
+#include <unistd.h>
+
+void actualize_ll(t_expr_ll *curr_expr)
+{
+	t_expr_ll *next_expr;
+
+	curr_expr->sign = curr_expr->next->sign;
+	next_expr = curr_expr->next->next;
+	free_expr(curr_expr->next);
+	curr_expr->next = next_expr;
+}
+
+bool	resolved_priority(t_expr_ll *expr_ll)
+{
+	t_expr_ll *ptr;
+
+	ptr = expr_ll;
+	while(ptr->next)
+	{
+		if (ptr->sign == MULT || ptr->sign == DIV)
+		{
+			if (ptr->sign == MULT)
+				ptr->value = ptr->value * ptr->next->value;
+			else
+				ptr->value = ptr->value / ptr->next->value;
+			actualize_ll(ptr);
+			return (true);
+		}
+		ptr = ptr->next;
+	}
+	return (false);
+}
 
 long long calculate(t_expr_ll *expr_ll)
 {
-	long long result;
-	t_expr_ll *expr;
+	t_expr_ll *ptr;
 
-	result = 0;
-	expr = expr_ll;
-	while(expr)
+	while(1)
+		if (!resolved_priority(expr_ll))
+			break ;
+	ptr = expr_ll;
+	while(ptr->next)
 	{
-		if (expr->sign == PLUS)
-			result = expr->value + expr->next->value;
-		else if (expr->sign == MINUS)
-			result = expr->value - expr->next->value;
-		else if (expr->sign == DIV)
-			result = expr->value / expr->next->value;
-		else if (expr->sign == MULT)
-			result = expr->value * expr->next->value;
-		expr= expr->next->next;
+		if (ptr->sign == PLUS)
+			ptr->value = ptr->value + ptr->next->value;
+		else
+			ptr->value = ptr->value - ptr->next->value;
+		actualize_ll(ptr);
 	}
-	return (result);
+	print_all_exprs(expr_ll);
+	return (ptr->value);
 }
